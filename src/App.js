803f77390576b3f0bfe740eabc513585951fcdcb
Stuart Wong <swong31@gmail.com>
2020-11-11 22:43:09 -0800

    Bringing in data from my axios request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Wrapper from "./components/Wrapper";
+import API from "./utils/API";
+
+function getEmployees() {
+  API.getEmployees().then(res => console.log(res.data.results)).catch(err => console.log(err));
+}
+getEmployees()
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper />
     </div>
   );
 }
