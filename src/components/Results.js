72e8c9858aee453b904895af8011aeda120d3cc9
Stuart Wong <swong31@gmail.com>
2020-11-12 16:44:24 -0800

    Added Results.js

---
+++ src/components/Results.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Results.css";
+
+function Results(props) {
+  return (
+    <ul className="list-group">
+      {props.results.map(result => (
+        <li className="list-group-item" key={result.id}>
+          <img alt={result.title} className="img-fluid" src={result.images.original.url} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Results;
